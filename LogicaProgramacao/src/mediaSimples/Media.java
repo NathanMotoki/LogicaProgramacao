950114ea88924262354359a99afd89f23bcc586a
Nathan Motoki <nathan_motoki@hotmail.com>
2021-10-11 11:52:03 -0300

    adicionando exercicios

--- LogicaProgramacao/src/mediaSimples/Media.java
+++ LogicaProgramacao/src/mediaSimples/Media.java
@@ -4,9 +4,14 @@ public class Media {
 
     public void CalcularMedia() {
         int[] lista = {1, 1, 1};
-        int cont;
+        int cont = 0;
+        int res;
 
         for (int i = 0; i <= lista.length; i++) {
+            lista[i] += cont;
         }
+
+        res = cont/lista.length;
+
     }
 }

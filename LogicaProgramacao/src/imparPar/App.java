5ee8aa3fffc3d03b241aae2c12080f037ff6dd0f
Nathan Motoki <nathan_motoki@hotmail.com>
2021-10-12 21:05:44 -0300

    Adicioando exercícios

---
+++ LogicaProgramacao/src/imparPar/App.java
@@ -0,0 +1,7 @@
+package imparPar;
+
+public class App {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ LogicaProgramacao/src/imparPar/descobrirValor.java
@@ -0,0 +1,5 @@
+package imparPar;
+
+public class descobrirValor {
+    
+}

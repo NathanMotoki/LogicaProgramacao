d76582aab063f20f1eedc25026dc35d0f35c9588
Nathan Motoki <nathan_motoki@hotmail.com>
2021-10-14 17:26:57 -0300

    adicionando exercicios

--- LogicaProgramacao/src/somarDigitos/App.java
+++ LogicaProgramacao/src/somarDigitos/App.java
@@ -2,6 +2,8 @@ package somarDigitos;
 
 public class App {
     public static void main(String[] args) {
-        
+        SomarDigitos valor = new SomarDigitos();
+
+        valor.somar(502);
     }
 }
--- LogicaProgramacao/src/somarDigitos/SomarDigitos.java
+++ LogicaProgramacao/src/somarDigitos/SomarDigitos.java
@@ -4,6 +4,15 @@ public class SomarDigitos {
     
     public void somar(int numero) {
         
+        int soma = 0;
+
+        while(numero>0) {
+            soma += (numero%10);
+            numero /= 10;
+        }
+
+        System.out.println("Somando seus digitos: " + soma);
+
     }
 
 }
